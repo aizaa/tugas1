6270994a0cbca5762090384923cd461face8f32a
aizaaseptii <aiza@g2academy.co>
2020-03-26 12:26:42 +0700

    tugas ke 2

--- tugas2.go
+++ tugas2.go
@@ -2,21 +2,32 @@ package main
 
 import "fmt"
 
-func main() {
-	mul := calc (16)
-
-	nilai := []int {21, 31, 41}
-	nilai[0] = 46
-	nilai[1] = 62
-	nilai[2] = 82
-
-
-	fmt.Println("multiply: ", mul)
-	fmt.Println(nilai)
+//soal pertama
+func print_func(anonymous_func string) string {
+	return anonymous_func
 }
 
-	func calc(a int) (int) {
-		multiply := a * 54 + 23 / 12 - 130
+func main(){
 
-		return multiply
+	//soal kedua
+	nilai := []int {45, 23, 56, 89, 900, 26, 78}
+	
+	var GanjilGenap = func(a ...int) ([]int, []int){
+	var ganjil = []int{}
+	var genap = []int{}
+
+		for _, a := range a {
+			if a%2 == 0 {
+				ganjil = append(ganjil, a)
+			}else{
+				genap = append(genap, a)
+			}
+		}
+		return ganjil, genap
 	}
+	var ganjil, genap = GanjilGenap(nilai...)
+	
+	fmt.Println("Ganjil: ", ganjil)
+	fmt.Println("Genap: ", genap)
+	fmt.Println(print_func("Hi there anonymous"))
+}
